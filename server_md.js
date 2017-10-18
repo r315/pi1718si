6832f657cfc17166d280125cbe6df76a3f47c998
hmr <hmr@ultrabook-u17.04>
2017-10-19 00:40:34 +0100

    ADD: export for get method and basic endpoint filtering

--- server_md.js
+++ server_md.js
@@ -1,15 +1,45 @@
 'use strict'
 
 let http = require('http')
+const endpoints = ['search', 'movies', 'actors']
+const TAG = 'Server'
+
+const logger = (msg) => {console.log(TAG + ': ' + msg)}
+
+function requestFail(resp, msg){
+    resp.writeHead(404,{'Content-Type': 'text/html'})
+    resp.write(msg)
+    resp.end()
+}
+
+function requestSuccess(req, resp){
+    resp.writeHead(200, {'Content-Type': 'text/html'});
+    resp.end()
+}
+
+function requestResponse(req, resp){
+    if(req.method == 'GET'){
+        let endpoint = req.url.split('/').splice(1)
+        if(!endpoints.includes(endpoint[0])){
+            let msg = `Endpoint not found \"/${endpoint[0]}\"`
+            logger(msg)
+            requestFail(resp,msg)
+            return
+        }
+        requestSuccess(req, resp)
+        logger(endpoint)
+    }
+}
 
 
-module.exports = {
-    
-    'init' : function (port){
-        http.createServer(
-            function (req, res) {
-                res.write('Hello World!');  //write a response to the client
-                res.end();                  //end the response
-        }).listen(port);                    //the server object listens on port 8080 
+module.exports = {    
+    'init' : function (port){http.createServer( requestResponse).listen(port); logger(`Started on ${port}`)},
+    'get' : function(url, callback){
+        http.get(url, (resp) => {
+            let buffer = ""
+            resp.on('data', (data) => buffer += data)  //TODO: improve this ?
+            resp.on('end', () => callback(buffer))
+            resp.on('error', (erro) => logger(error))
+        })
     }
 }
--- server_test.js
+++ server_test.js
@@ -4,3 +4,6 @@ let server = require('./server_md')
 
 
 server.init(8080)
+
+
+server.get('http://www.google.com', (data) => console.log(data))
