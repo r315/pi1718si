a0773be55ac366784081637bc0f7cc2a851c5bdf
Ricardo Goncalves <rgoncalvespro@gmail.com>
2017-10-22 22:55:10 +0100

    esqueleto incial do dispatcher .. falta decidir se vamos ter uma camada para verificacaos das caches ou se as caches vao ficar no dispatcher..  ideias ideias :)

---
+++ dispatcher.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+TODO: 
+imports dos modulos de criação para os endpoints necessarions 
+*/
+
+function dispatcher(entry,value){
+
+        if(entry==="movies")
+            /* chamada ao objecto movies */ 
+            console.log("not implemented")
+        if(entry === "search")
+            /*chamada a funcção de movie .. com parametros de pesquisa == */ 
+            console.log("not implemented")
+        if(entry === "actors")
+
+         /*chamada a funcção actor */
+         console.log("not implemented")
+
+}
