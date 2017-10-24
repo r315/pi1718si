ecde27561d04edba37f6cc6f4e302604b97f8a97
Ricardo Goncalves <rgoncalvespro@gmail.com>
2017-10-24 23:26:21 +0100

    cleaning up commit

--- movie.js
+++ movie.js
@@ -1,6 +1,5 @@
 'use strict'
 const fs =  require('fs')
-
  function movie() {
     this.title = null,
     this.id = null,
@@ -26,6 +25,7 @@ function createMovie(tocreateMovie){
     
 }
 
+
 /*
 fs.readFile('teste_jason_moviesearch.txt',function (err,data) {
     if(err) throw err
@@ -35,19 +35,4 @@ fs.readFile('teste_jason_moviesearch.txt',function (err,data) {
 */
 
 
-
-
-/*
-function searchMoviebyId(searchId){
-    let innerMovie =null
-    if(isNaN(searchId)) {
-           innerMovie= movieCache.map(searchId)[0]
-           if(innerMovie == null ){
-                // chamada a camada que vai fazer os pedidos a movie database
-           }
-            return innerMovie
-    }
-        throw("Not a movie ID") //confirmar esta sintax 
-
-}
-*/
+module.exports(createMovie)
