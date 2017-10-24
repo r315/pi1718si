56d8f49e7e04fd1f503715dbf7f3b4f4f9523186
hmr <hmr@ultrabook-u17.04>
2017-10-24 22:31:32 +0100

    Add Test for requesting data using object

--- server_test.js
+++ server_test.js
@@ -6,4 +6,13 @@ let server = require('./server_md')
 server.init(8080)
 
 
-server.get('http://www.google.com', (data) => console.log(data))
+//server.get('http://www.google.com', (data) => console.log(data))
+
+
+
+//server.get(tmdb_movie_detail, (data) => console.log(data))
+server.request({
+    'path': 'movies',
+    'id': 550,
+    'response': (data) => console.log(data)
+})
