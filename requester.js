eb694366e665b55b48c717ecc33b18b26827299b
hmr <hmr@ultrabook-u17.04>
2017-10-25 23:26:31 +0100

    ADD: requester movie

--- requester.js
+++ requester.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const MOVIE_DETAILS_SIZE = 2 
+
 const server = require('./server_md')
 
 
@@ -12,8 +14,34 @@ function searchByMovie(searchTerm,callbackfunc){
     }
 
 
+function searchByMovieId_joinner(rspdata, cb){
+    if(rspdata.count == MOVIE_DETAILS_SIZE)
+        cp(rspdata)
+}
+
+
+function searchByMovieId(searchId, cb){
+    let respdata = {
+        'movie' : '', 
+        'cast' : '',
+        'count' : 0
+    }
+
+    let reqmovie = {
+        'path':'movies',
+        'id' : searchId,
+        'response' : (data) => { respdata.movie = data; rspdata.count++; searchByMovieId_joinner(respdata, cb);}
+    }
+
+    let reqcast = {
+        'path':'movies',
+        'id' : searchId + '/credits',
+        'response' : (data) => { respdata.cast = data; rspdata.count++; searchByMovieId_joinner(respdata, cb);}
+    }
+}
 
 
 module.exports = { 
-    "searchByMovie" : searchByMovie
+    'searchByMovie' : searchByMovie,
+    'searchByMovieId' : searchByMovieId 
 }
