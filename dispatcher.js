b01ffd11dfcc6cb1b46f357952e388080bee9a91
hmr <hmr@ultrabook-u17.04>
2017-10-27 00:34:03 +0100

    Dispatcher proposal

--- dispatcher.js
+++ dispatcher.js
@@ -7,6 +7,33 @@ let entry = []
 /* Dispatche Assumes a two position array on entry */
 //:TODO Check with Hugo R.
 
+/* dispatcher receives one object as parameter composed on the following manner
+
+entry{
+    'resp' :     // response parameter from http server for sending data to client
+    'req' :      // requeste parameter from hrrp server containig client request
+    'endpoint':  // endpoint entered by client
+    'path':      // path can be [search | movies | actors]
+    'data' :     // data to return to client <html>
+    'response' : // call back when data is available to send to client
+}
+
+
+for routing, an object with functions assigned to all path's as properties can be used, this way we can simply 
+index the propertie and call the function
+
+router{
+    'search': function(){....},
+    'movies': function(){....},
+    'actors': function(){....},
+}
+
+then on dispatcher we can call
+router[entry.path](entry)
+
+as we must use async model the call to dispatcher returns immediatly and has no return value  
+*/
+
 function dispatcher(entry){
 
     switch (entry[0]) {
