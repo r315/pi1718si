12bcf13e883ede75e9b48a99d6dbec3effc3b969
Ricardo Goncalves <rgoncalvespro@gmail.com>
2017-11-25 19:08:12 +0000

    Added method to get a document form database by the ID of the document

--- CouchDb.js
+++ CouchDb.js
@@ -1,3 +1,5 @@
+
+
 'use strict'
 
 let dbreq = require('http') 
@@ -5,52 +7,28 @@ const serveraddress = 'localhost'
 const gport = 5984
 const database ='coimadb'
 
-const headerobj = {
-    'Content-Type' : 'application/json',
-    'Content-Length' : ''
-}
-let options = {
-    host : serveraddress,
-    port : gport,
-    path: `/${database}`,
-    method:'',
-    headers : ''
-
-}
-
-
-
-function reqFromDb(uri){
-    let rawData =''
-    
-   dbreq.get(`http://${serveraddress}:${gport}/${uri}`,(res) => {
-     
-        res.setEncoding('utf8')
-        console.log("Started")
-       
-        res.on('data',(chunk) => {rawData += chunk 
-      
-    })
-        
-        
-       res.on('end',()  => {console.log(rawData)})
-    })
 
     
-    
-    
-}
-    
+//function setupReq(body,type){
+function setupReq(reqparams){
 
-function setupReq(body,type){
+    let options = {
+        host : serveraddress,
+        port : gport,
+        path: `/${database}`,
+        method:'',
+        headers : ''
+    
+    }
+
     const headerobj = {
         'Content-Type' : 'application/json',
-        'Content-Length' : Buffer.byteLength(JSON.stringify(body))
+        'Content-Length' : Buffer.byteLength(JSON.stringify(reqparams.body))
     }
-    
-            options.headers =headerobj
-            options.method = type
-       let post_req= dbreq.request(options,function(res){
+    options.path = (reqparams.path ===undefined)?options.path : options.path+reqparams.path
+    options.headers = headerobj
+    options.method  = reqparams.type
+    let post_req= dbreq.request(options,function(res){
            res.setEncoding('utf8');
            res.on('data',function(chunk){
                console.log('Response:',chunk)
@@ -61,41 +39,72 @@ function setupReq(body,type){
 }
 
 function insertDocOnDb(indoc){
-        
+   let reqparams = {
+        body : indoc,
+        type : "POST"
+    }
     
-// const headerobj = {
-//     'Content-Type' : 'application/json',
-//     'Content-Length' : Buffer.byteLength(JSON.stringify(indoc))
-// }
-
-//         options.headers =headerobj
-//         options.method = 'POST'
-//    let post_req= dbreq.request(options,function(res){
-//        res.setEncoding('utf8');
-//        res.on('data',function(chunk){
-//            console.log('Response:',+chunk)
-//        })
-//    })
-    let post_reqi = setupReq(indoc,'POST')
-
-   post_reqi.write(JSON.stringify(indoc))
-   
-   post_reqi.on('error', err =>{
+    
+    let post_reqi = setupReq(reqparams)
+    post_reqi.write(JSON.stringify(indoc))
+    post_reqi.on('error', err =>{
     console.log("error on post to database",err.message)
 })
 
-    //post_reqi.on('close',console.log('Done'))
+    
    post_reqi.end()
     
 }
 
 
+function getDocbyid(id){
+    let rawData=''
+    let reqparams = {
+        body : "",
+        type : "GET",
+        path :`/_design/login/_view/byid?key="${id}"`
+    }
+     let options = {
+        host : serveraddress,
+        port : gport,
+        path: `/${database}`,
+        method:'',
+        headers : ''
+    }
+    const headerobj = {
+        'Content-Type' : 'application/json',
+    }
+    options.path = (reqparams.path ===undefined)?options.path : options.path+reqparams.path
+    options.headers = headerobj
+    dbreq.get(options,(res) => {
+        res.setEncoding('utf8')
+        console.log("Started")
+        res.on('data',(chunk) => {rawData += chunk 
+    })
+       res.on('end',()  => {
+           console.log(JSON.parse(rawData).rows[0].value)  
+         // return JSON.parse(rawData).rows[0].value 
+        })
+    })
+}
+
+
+//preciso de saber o que procurar 
+// se id ?  e nome 
+//Sugestão de obj 
+//{searchfield: "",searchvalue:"",[CBFN]}
+function updateDocOnDB(id){
+    console.log(getDocbyid(id))
+}
+
 //reqFromDb("_all_dbs")
 var sampleDoc ={
-    "movie": "matrix",
-    "date":  "2001", 
+    "movie": "bladerunner2049",
+    "date":  "2009", 
     "type": "movie"
 }
 
 
-insertDocOnDb(sampleDoc)
+//insertDocOnDb(sampleDoc)
+//getDocbyid("69314fa07586f554110474cfd300c1f9")
+updateDocOnDB("69314fa07586f554110474cfd300c1f9")
