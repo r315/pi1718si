d8ce4731ba599f83ecd69888e43fb8c026333f3d
r315 <38652@alunos.isel.ipl.pt>
2017-11-27 22:52:02 +0000

    wip express passport

--- app/MidlewareUser.js
+++ app/MidlewareUser.js
@@ -5,6 +5,7 @@ let fs = require('fs')
 let hb = require('handlebars')
 let bodyparser = require('body-parser')
 let cookieParser = require('cookie-parser')
+let passport = require('passport')
 
 const logger = (msg) => {console.log('Midleware User: ' + msg); return msg;}
 const TEMPLATE_FILE_USER = 'templateviews/user.hbs'
@@ -86,6 +87,7 @@ function userProfile(req, resp, next){
 router.use(cookieParser())
 router.use('/:id',userProfile)
 router.get('/login',userLogin)
-router.post('/login', bodyparser.urlencoded({ extended: false }), userLoginRequest)
+//router.post('/login', bodyparser.urlencoded({ extended: false }), userLoginRequest)
+router.post('/login', passport.authenticate('local'),userLoginRequest)
 
 module.exports = router
