9b5a94c593eb6106f354bb2a030341c68629e317
r315 <38652@alunos.isel.ipl.pt>
2017-11-29 01:09:44 +0000

    wip login

--- app/MidlewareLogin.js
+++ app/MidlewareLogin.js
@@ -11,6 +11,41 @@ router.get('/', (req, resp, next)=>{
     resp.render('login',{'title':'Title'})
 })
 router.post('/', bodyparser.urlencoded({ extended: false }), (req, resp, next)=>{
+
+/*
+    function authenticateUser (username, password, cb) {
+        if (username !== 'palbp' || password !== 'penta') {
+            cb(new Error('Invalid credentials'), null)
+            return
+        }
+
+        cb(null, {
+            username: 'palbp',
+            email: 'palbp@cc.isel.ipl.pt'
+        })
+    }
+
+    authenticateUser(req.body.username, req.body.password, (err, user) => {
+        if (err) { next(err); return }
+
+        req.logIn(user, (error) => {
+            if (error) { next(error); return }
+            res.redirect('/')
+        })
+    })
+}
+
+*/
+
+
+
+         // check credentials with database
+        // add session id to logged collection
+        loggedusers.push(req.cookies)
+        // if cookie with login info 
+        // redirect to user page
+        resp.end()
+
     resp.render('login',{'title':'Title'})
 })
 
--- app/MidlewareUser.js
+++ app/MidlewareUser.js
@@ -7,70 +7,7 @@ let bodyparser = require('body-parser')
 let cookieParser = require('cookie-parser')
 
 const logger = (msg) => {console.log('Midleware User: ' + msg); return msg;}
-const TEMPLATE_FILE_USER = 'templateviews/user.hbs'
-const TEMPLATE_FILE_LOGIN = 'templateviews/login.hbs'
-const LOGIN_PATH = '/users/login'
-
-/**
- * 
- * @param {*} req 
- * @param {*} resp 
- */
-function userLogin(req, resp){
-    fs.readFile(TEMPLATE_FILE_LOGIN, function(error, data){
-        let source = data.toString()
-        let template = hb.compile(source) 
-
-        let dataobj = {
-            'login_link' : LOGIN_PATH
-        }
-        resp.send(template(dataobj))        
-    })
-    
-   
-}
-
-/**
- * 
- * @param {*} req 
- * @param {*} resp 
- */
-function userLoginRequest(req, resp){
-
-/*
-    function authenticateUser (username, password, cb) {
-        if (username !== 'palbp' || password !== 'penta') {
-            cb(new Error('Invalid credentials'), null)
-            return
-        }
-
-        cb(null, {
-            username: 'palbp',
-            email: 'palbp@cc.isel.ipl.pt'
-        })
-    }
-
-    authenticateUser(req.body.username, req.body.password, (err, user) => {
-        if (err) { next(err); return }
-
-        req.logIn(user, (error) => {
-            if (error) { next(error); return }
-            res.redirect('/')
-        })
-    })
-}
-
-*/
-
-
-
-         // check credentials with database
-        // add session id to logged collection
-        loggedusers.push(req.cookies)
-        // if cookie with login info 
-        // redirect to user page
-        resp.end()
-}
+const COOKIE_NAME = 'login-info'
 
 /**
  * 
@@ -87,10 +24,11 @@ function userProfile(req, resp, next){
         next()       
     }
 
-    if(cookie['login-info'] == undefined || req.params.id != cookie.username){
+    if(cookie['login-info'] == undefined || req.params.id != cookie[COOKIE_NAME].username
+){
         //user tried to access his user area without login first
         let  username = req.params.id //newUser()
-        resp.cookie('login-info',{'username' : username} )// createUser(username))
+        resp.cookie(COOKIE_NAME,{'username' : username} )// createUser(username))
         resp.redirect('/login')
     }
     next()
@@ -102,10 +40,5 @@ router.get('/', (req,resp,next) => {
 })
 
 router.use('/:id', userProfile)
-router.get('/:id/lists',(req, resp, next) => {
-    next()
-})
-
-//router.post('/login', userLoginRequest)
 
 module.exports = router
