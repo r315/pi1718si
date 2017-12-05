feb6a26a0589d80d883574fb2f611e03d6df4971
Ricardo Goncalves <rgoncalvespro@gmail.com>
2017-12-05 23:01:13 +0000

    Changed function update user , main fucntion updateddocondb  and update favlist

--- app/CouchDb.js
+++ app/CouchDb.js
@@ -60,14 +60,15 @@ function insertDocOnDb(indoc,cbf){
  * @param {*} cbf callback function to be called when update is concluded 
  */
 function updateDoconDB(indoc,key,cbf){
-    let respdata =''
+    let parsedresponse =''
     //TODO:  correct this remove reqparams structure
-
+    /*
     let reqparams = {
         body : indoc,
         type : "PUT"
     }
-    
+    */
+    let respdata =''
     let options = {
        host : serveraddress,
        port : gport,
@@ -78,13 +79,13 @@ function updateDoconDB(indoc,key,cbf){
    }
    const headerobj = {
     'Content-Type' : 'application/json',
-    'Content-Length' : Buffer.byteLength(JSON.stringify(reqparams.body))
+    'Content-Length' : Buffer.byteLength(JSON.stringify(indoc))
     }
 
    // if indoc tem campo id , se n~ºao tiver não pode fazer o update
    options.path = options.path+`/${key}`
    options.headers = headerobj
-   options.method = reqparams.type
+   options.method = 'PUT'
    let inner_req = dbreq.request(options,
             function(res) {
                 res.setEncoding('utf8')
@@ -92,8 +93,14 @@ function updateDoconDB(indoc,key,cbf){
                     respdata += chunk 
                    console.log('DBACCESS: Status on update',`${res.statusCode}-${res.statusMessage}`)
                 })
-                res.on('end',()=> cbf(JSON.parse(respdata)))
-
+                res.on('end', ()=> {
+                    parsedresponse = JSON.parse(respdata)
+                    if(parsedresponse.id == undefined){
+                        cbf(parsedresponse,null)
+                    }else{
+                        cbf(null,parsedresponse)
+                    }
+                })
             })
      inner_req.write(JSON.stringify(indoc))
      inner_req.end()
@@ -220,17 +227,19 @@ function inner_searchbyusername(user,cbf){
     }
 }
 
+
 /**
  * fucntion used to search by user id instead of login 
  * @param {*} tocheckid id of the user to be searched 
  * @param {*} cbf call back fucntion to return 
  */
+/*
 function searchbyuserid(tocheckid,cbf){
 
     getDoc(tocheckid,'/_design/login/_view/byid',cbf)
 
 }
-
+**/
 /**
  * internal funcntion to the module  searchbyuserid , cvalidates teh user and returns 
  * @param {*} userid userid that was being searched 
@@ -249,6 +258,7 @@ function inner_searchbyuserid(userid,cbf){
  * @param {*} user username to search
  * @param {*} cbf callback function to return back
  */
+/*
 function inner_insertUser(data,user,cbf){
     if(JSON.parse(data).rows.length  == 1){
        logger('insert of user user exists') // devolvido erro 
@@ -257,7 +267,7 @@ function inner_insertUser(data,user,cbf){
          insertDocOnDb(user,cbf)
     }
 }
-
+*/
 
 /**
  * internal function to the module update user to be used to validate after searching the user to update 
@@ -265,13 +275,14 @@ function inner_insertUser(data,user,cbf){
  * @param {*} user userobject to update 
  * @param {*} cbf callback function to return after the update is done 
  */
-function inner_updateUser(data,user,cbf){
-    if(JSON.parse(data).rows.length == 0){
+function inner_updateUser(error,data,user,cbf){
+    if(data==null){
         logger ("ERROR on user update: user does not exist")
+        cbf(error,null)
     }else{
-        user._rev=JSON.parse(data).rows[0].value._rev
+        user._rev=data._rev
         user.type = "user"
-        updateDoconDB(user,cbf)
+        updateDoconDB(user,user.name,cbf)
     }
 }
 
@@ -295,7 +306,8 @@ function insertUser(userobj,cbf){
  * @param {*} cbf callback function to be used 
  */
 function updateUser(userobj,cbf){
-    searchbyuserid(userobj.id,(data)=>inner_updateUser(data,userobj,cbf))
+    getDoc(userobj.name,(error,data)=>inner_updateUser(error,data,userobj,cbf))
+
 }
 
 /**
@@ -325,7 +337,7 @@ function searchbylistid(listid,cbf){
  * @param {*} cbf call back funtion 
  */
 function updatefavlist(listobj,cbf){
-    searchbylistid(listobj.id,(data)=>inner_updatelist(data,listobj,cbf))
+    getDoc(listobj.id,(error,data)=>inner_updatelist(error,data,listobj,cbf))
 }
 
 /**
@@ -334,13 +346,14 @@ function updatefavlist(listobj,cbf){
  * @param {*} list list object that is updated 
  * @param {*} cbf callback function when finished
  */
-function inner_updatelist(data,list,cbf){
-    if(JSON.parse(data).rows.length == 0){
+function inner_updatelist(error,data,list,cbf){
+    if(data == null){
          logger ("ERROR on list update: list does not exist")
+         cbf(error,null)
     }else{
-        list._rev =JSON.parse(data).rows[0].value._rev
+        list._rev =data._rev
         list.type="favlist"
-        updateDoconDB(list,cbf)
+        updateDoconDB(list,list.id,cbf)
     }
 }
 
@@ -400,47 +413,51 @@ function inner_deletelist(data,list,cbf){
  */
 function outfunctest(error,sample){ 
     //console.log(`going through test FUNCTION callback: ${sample}`) 
-    console.log(sample) 
-    console.log(error)
+    console.log('message:'+JSON.stringify(sample)) 
+    console.log('ERROR:'+JSON.stringify(error))
 } 
 
 
 
 var sampleDocuser ={
-    id: "69314fa07586f554110474cfd302d7d6",
+    
     name: "potatohead",
-    passwd : "dummy", 
+    passwd : "dummy3", 
     lists : []
 }
 
 
 var sampleList ={
-   
+   id:"69314fa07586f554110474cfd30347d1",
     name: "Sci-fi",
     movies : [],
 }
 
 var tmovie = {
-    title: "nightmare ",
+    title: "star wars ",
     id: 1
 }
 
-//sampleList.movies.push(tmovie)
+var movie2 = {
+    title : "Rambo",
+    id:2
+}
+sampleList.movies.push(tmovie)
+sampleList.movies.push(movie2)
 //deleteList(sampleList,outfunctest)
 //deleteUser(sampleDocuser,outfunctest)
-//insertfavlist(sampleList,outfunctest)
+//insertfavlist(sampleList,outfunctest) T
 //searchbylistid("69314fa07586f554110474cfd303392d",outfunctest)
-//updateList(sampleList,outfunctest)
-//searchbyusername("potatohead",outfunctest) 
+//updatefavlist(sampleList,outfunctest) T
+//searchbyusername('potatohead',outfunctest) T
 //insertDocOnDb(sampleDoc) t
-//getDocbyid("69314fa07586f554110474cfd300c1f9")
-//updateUser(sampleDoc,outfunctest)
+//getDocbyid("69314fa07586f554110474cfd300c1f9") to delete
+//updateUser(sampleDocuser,outfunctest) missing test
 //insertUser(sampleDocuser,outfunctest) t
 module.exports = {
-    'searchbyuser'  : searchbyusername,
-    'searchbyuserid': searchbyuserid,
+    'searchByUser'  : searchbyusername,
     'insertuser'    : insertUser,
-    'updateUser'    : updateUser,
+    //'updateUser'    : updateUser,
     'insertfavlist' : insertfavlist,
     'updatefavlist' : updatefavlist,
     'searchbylistid': searchbylistid
