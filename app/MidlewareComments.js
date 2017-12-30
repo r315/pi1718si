ccd4856d43ba5619bba73039ef81f81066b24dcf
r315 <38652@alunos.isel.ipl.pt>
2017-12-30 22:27:13 +0000

    ADD post and get comments for movie id

--- app/MidlewareComments.js
+++ app/MidlewareComments.js
@@ -3,19 +3,37 @@
 const router = require('express').Router()
 const couchdb = require('./CouchDb')
 const bodyparser = require('body-parser')
+const Movie = require('./movie').movie
 
 const logger = (msg) => {console.log('Midleware Comments: ' + msg); return msg;}
 
-function postComment(comment, cb){
-    couchdb.postComment(comment, cb)
-}
-
-
 router.get('/', (req, resp) => { 
-    logger(req.baseUrl)
-    couchdb.getComments(req.movieid, 0, 5, (error, cmnts)=>{       
-        resp.send(error ? error : cmnts)
-    })   
+    let movieid = req.baseUrl.split('/')[2]  //  /movies/{id}/comments    
+    let comments = []
+
+    logger(`getting comments for id ${movieid}`)
+
+    function collectComments(comment, requiredsize){
+        comments.push(comment)
+        if(comments.length == requiredsize){
+            resp.send(comments)
+        }
+    }
+
+    couchdb.getMovie(movieid, (error, cmovie) =>{
+        if(error){      
+            resp.send("[]")     // no comments    
+            return
+        }
+        cmovie.comments.forEach( cmntid => {
+            couchdb.getComments(cmntid, 0, 5, (error, cmnt)=>{       
+                    if(error){
+                        logger(`Error ${error.error}`)
+                    }
+                    collectComments(cmnt, cmovie.comments.length)
+            })            
+        })
+    })      
 })
 
 /**
@@ -23,10 +41,25 @@ router.get('/', (req, resp) => {
  * so body parser must apply the correct encoding
  */
 router.post('/', bodyparser.json(), (req, resp) => {
-    couchdb.postComment(req.body, (error, comment) => {
-        resp.send(error ? error : comment)  // return comment with id
-        couchdb.getMovie(req.movieid, (error, cmovie) =>{
-            
+    logger(`Posting comment for movie ${req.movieid}`)
+    let movie = new Movie()
+    movie.id = req.body.movie_id
+    movie.title = req.body.movie_title 
+
+    couchdb.postComment(req.body.movie_comment, (error, comment) => {
+        if(error){
+            resp.send(error)
+            return
+        }
+        couchdb.createOnGetMovie(movie,(error, cmovie) =>{
+            if(error){
+                resp.send(error)
+                return
+            }
+            cmovie.comments.push(comment.id)
+            couchdb.updateMovie(cmovie, (error, cmovie)=>{                
+                resp.send(error ? error.reason : comment)  // return comment with id
+            })            
         })
     })    
 })
